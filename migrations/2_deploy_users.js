bd120d9a4446afe22e01e7c9945df1791f8d613a
Ian-Bright <ianbrighton1@gmail.com>
2021-01-12 14:50:26 -0500

    consumer is funded during deployment

--- migrations/2_deploy_users.js
+++ migrations/2_deploy_users.js
@@ -6,7 +6,6 @@ const name = "MUBC Admin"
 const twitterId = "1076569795376676867"
 const imageUrl = "AOh14GgHdiwS3EV4d5EXfbXC4mNcEONbthxkBNQllmjz"
 
-module.exports = async (deployer) => {
+module.exports = async (deployer, network, address) => {
     await deployer.deploy(UsersContract, name, twitterId, imageUrl, {gas: 6720000, overwrite: false})
-    // const users = await UsersContract.deployed()
 }
--- migrations/6_dummy_data.js
+++ migrations/6_dummy_data.js
@@ -1,55 +1,55 @@
-const UsersContract = artifacts.require("Users")
-const AnnouncementsContract = artifacts.require("Announcements")
-const ItemsContract = artifacts.require("Items")
-const BountiesContract = artifacts.require("Bounties")
+// const UsersContract = artifacts.require("Users")
+// const AnnouncementsContract = artifacts.require("Announcements")
+// const ItemsContract = artifacts.require("Items")
+// const BountiesContract = artifacts.require("Bounties")
 
-module.exports = async (deployer, network, accounts) => {
-    if (true) {
+// module.exports = async (deployer, network, accounts) => {
+//     if (true) {
 
-        // get deployed contracts
-        const users = await UsersContract.deployed()
-        const announcements = await AnnouncementsContract.deployed()
-        const items = await ItemsContract.deployed()
-        const bounties = await BountiesContract.deployed()
+//         // get deployed contracts
+//         const users = await UsersContract.deployed()
+//         const announcements = await AnnouncementsContract.deployed()
+//         const items = await ItemsContract.deployed()
+//         const bounties = await BountiesContract.deployed()
 
-        // deploy users
-        let admin1 = accounts[0]
-        let user1 = accounts[3]
-        let user2 = accounts[4]
-        let user3 = accounts[5]
-        let image = 'AOh14GgHdiwS3EV4d5EXfbXC4mNcEONbthxkBNQllmjz' //not doing different photos
+//         // deploy users
+//         let admin1 = accounts[0]
+//         let user1 = accounts[3]
+//         let user2 = accounts[4]
+//         let user3 = accounts[5]
+//         let image = 'AOh14GgHdiwS3EV4d5EXfbXC4mNcEONbthxkBNQllmjz' //not doing different photos
 
-        await users.enroll("User 1", "", image, { from: user1 })
-        await users.enroll("User 2", "", image, { from: user2 })
-        await users.enroll("User 3", "", image, { from: user3 })
+//         await users.enroll("User 1", "", image, { from: user1 })
+//         await users.enroll("User 2", "", image, { from: user2 })
+//         await users.enroll("User 3", "", image, { from: user3 })
 
-        // deploy items
-        await items.addItem("Item 1", "This is the body for Item 1!", "", 10, true, 0, { from: admin1 })
-        await items.addItem("Item 2", "This is the body for Item 2!", "", 20, false, 3, { from: admin1 })
+//         // deploy items
+//         await items.addItem("Item 1", "This is the body for Item 1!", "", 10, true, 0, { from: admin1 })
+//         await items.addItem("Item 2", "This is the body for Item 2!", "", 20, false, 3, { from: admin1 })
 
-        // deploy bounties
-        await bounties.addBounty(
-            'Bounty 1',
-            'This is the description for the first manual bounty.',
-            "",
-            100,
-            true,
-            0,
-            true,
-            "",
-            { from: admin1 }
-        )
-        await bounties.addBounty(
-            'Bounty 2',
-            'This is the description for the second manual bounty.',
-            "",
-            100,
-            false,
-            3,
-            true,
-            "",
-            { from: admin1 }
-        )
-    }
+//         // deploy bounties
+//         await bounties.addBounty(
+//             'Bounty 1',
+//             'This is the description for the first manual bounty.',
+//             "",
+//             100,
+//             true,
+//             0,
+//             true,
+//             "",
+//             { from: admin1 }
+//         )
+//         await bounties.addBounty(
+//             'Bounty 2',
+//             'This is the description for the second manual bounty.',
+//             "",
+//             100,
+//             false,
+//             3,
+//             true,
+//             "",
+//             { from: admin1 }
+//         )
+//     }
 
-}
+// }
